67cbcf31287b41a19ec7484c5d2d3fe4ee8e3d64
Christoph Hochreiner <ch.hochreiner@gmail.com>
2015-07-16 17:49:33 +0200

    adopt message structure

--- src/main/java/vispDataProvider/service/HelloWorldService.java
+++ src/main/java/vispDataProvider/service/HelloWorldService.java
@@ -3,7 +3,6 @@ package vispDataProvider.service;
 import entities.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -18,8 +17,9 @@ public class HelloWorldService {
 
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
 
-        AmqpTemplate template = new RabbitTemplate(connectionFactory);
-        template.convertAndSend(QUEUE_NAME, msg);
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setRoutingKey(QUEUE_NAME);
+        template.convertAndSend(msg);
         connectionFactory.destroy();
     }
 }
