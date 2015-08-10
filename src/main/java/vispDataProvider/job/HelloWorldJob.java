8499a59ab059aee87eca47fdb142f4e48d8a1894
Christoph Hochreiner <ch.hochreiner@gmail.com>
2015-08-10 14:05:21 +0200

    added taxidata

--- src/main/java/vispDataProvider/job/HelloWorldJob.java
+++ src/main/java/vispDataProvider/job/HelloWorldJob.java
@@ -2,6 +2,7 @@ package vispDataProvider.job;
 
 
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import vispDataProvider.service.HelloWorldService;
@@ -13,6 +14,13 @@ public class HelloWorldJob implements Job {
 
         @Override
         public void execute(JobExecutionContext jobExecutionContext) {
-            service.hello();
+            JobDataMap jdMap = jobExecutionContext.getJobDetail().getJobDataMap();
+            String lastTime = "111";
+            if (jdMap.get("lastTime") != null) {
+                lastTime = jdMap.get("lastTime").toString();
+            }
+
+            lastTime = service.hello(lastTime);
+            jobExecutionContext.getJobDetail().getJobDataMap().put("lastTime", lastTime);
         }
 }
--- src/main/java/vispDataProvider/service/HelloWorldService.java
+++ src/main/java/vispDataProvider/service/HelloWorldService.java
@@ -1,25 +1,73 @@
 package vispDataProvider.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import entities.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import entities.Location;
+
+import java.sql.*;
 
 @Service
 public class HelloWorldService {
 
     private static final Logger LOG = LoggerFactory.getLogger(HelloWorldService.class);
-    private static final String QUEUE_NAME = "helloWorld";
-    public void hello() {
-        Message msg = new Message("Hello World");
 
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+    @Value("${rabbitMQ.outgoingQueue}")
+    private String QUEUE_NAME;
 
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setRoutingKey(QUEUE_NAME);
-        template.convertAndSend(msg);
-        connectionFactory.destroy();
+    @Value("${spring.rabbitmq.host}")
+    private String RABBITMQ_HOST;
+
+    public String hello(String lastDate) {
+        Connection connect = null;
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(RABBITMQ_HOST);
+
+        try {
+            Location location = new Location();
+
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/taxiData","root", "password");
+
+            statement = connect.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM Taxi WHERE time = (SELECT time FROM Taxi WHERE time > \"" + lastDate + "\" ORDER BY time ASC LIMIT 1)");
+
+            while (resultSet.next()) {
+                location = new Location(resultSet.getString("id"), resultSet.getString("taxiId"), resultSet.getString("time"), resultSet.getString("latitude"), resultSet.getString("longitude"));
+
+                ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+                Message msg = new Message(ow.writeValueAsString(location));
+
+                RabbitTemplate template = new RabbitTemplate(connectionFactory);
+                template.setRoutingKey(QUEUE_NAME);
+                template.setQueue(QUEUE_NAME);
+                template.convertAndSend(msg);
+                LOG.info("Message sent: " + msg.toString());
+
+            }
+            connectionFactory.destroy();
+
+            return location.getTime();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return lastDate;
     }
+
 }
