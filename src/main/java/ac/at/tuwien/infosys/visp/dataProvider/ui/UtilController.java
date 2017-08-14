f47965c0acad33339a763d1ffe47a42567839cb4
Christoph Hochreiner <ch.hochreiner@gmail.com>
2017-08-14 20:23:31 +0200

    Migrated to Bootstrap 4

--- src/main/java/ac/at/tuwien/infosys/visp/dataProvider/ui/UtilController.java
+++ src/main/java/ac/at/tuwien/infosys/visp/dataProvider/ui/UtilController.java
@@ -1,6 +1,9 @@
 package ac.at.tuwien.infosys.visp.dataProvider.ui;
 
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import ac.at.tuwien.infosys.visp.dataProvider.entities.CreateTaskForm;
 import ac.at.tuwien.infosys.visp.dataProvider.entities.EndpointConfiguration;
 import ac.at.tuwien.infosys.visp.dataProvider.util.EndpointConfigurationService;
@@ -13,10 +16,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UtilController {
@@ -97,7 +101,6 @@ public class UtilController {
     public String configurationSaved(@ModelAttribute EndpointConfiguration form, Model model, HttpServletResponse response) throws SchedulerException {
 
         ecs.storeData(form);
-
 
         model.addAttribute("endpointConfiguration", ecs.getConfiguration());
         model.addAttribute("message", "The configuration has been updated.");
