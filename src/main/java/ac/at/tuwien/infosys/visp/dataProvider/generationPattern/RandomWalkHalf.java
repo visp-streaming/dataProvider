147edc352e373c3f5a7193989a7b6c418bcd77d8
Christoph Hochreiner <chochreiner@users.noreply.github.com>
2017-09-04 09:05:58 +0200

    Randomwalk (#5)
    Add Random walk pattern

---
+++ src/main/java/ac/at/tuwien/infosys/visp/dataProvider/generationPattern/RandomWalkHalf.java
@@ -0,0 +1,241 @@
+package ac.at.tuwien.infosys.visp.dataProvider.generationPattern;
+
+
+import java.util.HashMap;
+
+import ac.at.tuwien.infosys.visp.dataProvider.entities.GenerationState;
+
+public class RandomWalkHalf implements GenerationPattern{
+
+    public GenerationState iterate(GenerationState state) {
+
+        HashMap<Integer, Double> states = new HashMap<>();
+        states.put(1, 2.0);
+        states.put(2, 2.0);
+        states.put(3, 2.0);
+        states.put(4, 3.0);
+        states.put(5, 2.0);
+        states.put(6, 2.0);
+        states.put(7, 2.0);
+        states.put(8, 2.0);
+        states.put(9, 2.0);
+        states.put(10, 2.0);
+        states.put(11, 2.0);
+        states.put(12, 3.0);
+        states.put(13, 3.0);
+        states.put(14, 3.0);
+        states.put(15, 3.0);
+        states.put(16, 3.0);
+        states.put(17, 2.0);
+        states.put(18, 2.0);
+        states.put(19, 2.0);
+        states.put(20, 2.0);
+        states.put(21, 3.0);
+        states.put(22, 3.0);
+        states.put(23, 3.0);
+        states.put(24, 2.0);
+        states.put(25, 2.0);
+        states.put(26, 2.0);
+        states.put(27, 3.0);
+        states.put(28, 3.0);
+        states.put(29, 3.0);
+        states.put(30, 3.0);
+        states.put(31, 3.0);
+        states.put(32, 3.0);
+        states.put(33, 3.0);
+        states.put(34, 3.0);
+        states.put(35, 2.0);
+        states.put(36, 2.0);
+        states.put(37, 2.0);
+        states.put(38, 1.0);
+        states.put(39, 1.0);
+        states.put(40, 2.0);
+        states.put(41, 2.0);
+        states.put(42, 2.0);
+        states.put(43, 2.0);
+        states.put(44, 3.0);
+        states.put(45, 3.0);
+        states.put(46, 3.0);
+        states.put(47, 2.0);
+        states.put(48, 2.0);
+        states.put(49, 2.0);
+        states.put(50, 2.0);
+        states.put(51, 3.0);
+        states.put(52, 3.0);
+        states.put(53, 3.0);
+        states.put(54, 3.0);
+        states.put(55, 3.0);
+        states.put(56, 3.0);
+        states.put(57, 3.0);
+        states.put(58, 3.0);
+        states.put(59, 3.0);
+        states.put(60, 3.0);
+        states.put(61, 2.0);
+        states.put(62, 2.0);
+        states.put(63, 2.0);
+        states.put(64, 2.0);
+        states.put(65, 2.0);
+        states.put(66, 2.0);
+        states.put(67, 2.0);
+        states.put(68, 2.0);
+        states.put(69, 2.0);
+        states.put(70, 2.0);
+        states.put(71, 2.0);
+        states.put(72, 1.0);
+        states.put(73, 2.0);
+        states.put(74, 1.0);
+        states.put(75, 1.0);
+        states.put(76, 1.0);
+        states.put(77, 1.0);
+        states.put(78, 1.0);
+        states.put(79, 1.0);
+        states.put(80, 1.0);
+        states.put(81, 1.0);
+        states.put(82, 1.0);
+        states.put(83, 1.0);
+        states.put(84, 1.0);
+        states.put(85, 0.0);
+        states.put(86, 1.0);
+        states.put(87, 1.0);
+        states.put(88, 1.0);
+        states.put(89, 1.0);
+        states.put(90, 1.0);
+        states.put(91, 1.0);
+        states.put(92, 1.0);
+        states.put(93, 1.0);
+        states.put(94, 2.0);
+        states.put(95, 1.0);
+        states.put(96, 1.0);
+        states.put(97, 1.0);
+        states.put(98, 1.0);
+        states.put(99, 1.0);
+        states.put(100, 1.0);
+        states.put(101, 1.0);
+        states.put(102, 1.0);
+        states.put(103, 1.0);
+        states.put(104, 1.0);
+        states.put(105, 1.0);
+        states.put(106, 1.0);
+        states.put(107, 1.0);
+        states.put(108, 1.0);
+        states.put(109, 0.0);
+        states.put(110, 0.0);
+        states.put(111, 0.0);
+        states.put(112, 0.0);
+        states.put(113, 0.0);
+        states.put(114, 0.0);
+        states.put(115, 0.0);
+        states.put(116, 0.0);
+        states.put(117, 0.0);
+        states.put(118, 1.0);
+        states.put(119, 1.0);
+        states.put(120, 1.0);
+        states.put(121, 1.0);
+        states.put(122, 1.0);
+        states.put(123, 1.0);
+        states.put(124, 0.0);
+        states.put(125, 0.0);
+        states.put(126, 0.0);
+        states.put(127, 0.0);
+        states.put(128, 0.0);
+        states.put(129, 0.0);
+        states.put(130, 0.0);
+        states.put(131, 0.0);
+        states.put(132, 1.0);
+        states.put(133, 1.0);
+        states.put(134, 1.0);
+        states.put(135, 1.0);
+        states.put(136, 2.0);
+        states.put(137, 1.0);
+        states.put(138, 1.0);
+        states.put(139, 1.0);
+        states.put(140, 1.0);
+        states.put(141, 1.0);
+        states.put(142, 1.0);
+        states.put(143, 2.0);
+        states.put(144, 2.0);
+        states.put(145, 2.0);
+        states.put(146, 1.0);
+        states.put(147, 1.0);
+        states.put(148, 1.0);
+        states.put(149, 1.0);
+        states.put(150, 2.0);
+        states.put(151, 1.0);
+        states.put(152, 1.0);
+        states.put(153, 1.0);
+        states.put(154, 1.0);
+        states.put(155, 1.0);
+        states.put(156, 1.0);
+        states.put(157, 1.0);
+        states.put(158, 1.0);
+        states.put(159, 1.0);
+        states.put(160, 0.0);
+        states.put(161, 0.0);
+        states.put(162, 0.0);
+        states.put(163, 0.0);
+        states.put(164, 0.0);
+        states.put(165, 0.0);
+        states.put(166, 0.0);
+        states.put(167, 1.0);
+        states.put(168, 1.0);
+        states.put(169, 1.0);
+        states.put(170, 1.0);
+        states.put(171, 1.0);
+        states.put(172, 0.0);
+        states.put(173, 1.0);
+        states.put(174, 1.0);
+        states.put(175, 1.0);
+        states.put(176, 1.0);
+        states.put(177, 1.0);
+        states.put(178, 1.0);
+        states.put(179, 0.0);
+        states.put(180, 0.0);
+        states.put(181, 0.0);
+        states.put(182, 0.0);
+        states.put(183, 0.0);
+        states.put(184, 0.0);
+        states.put(185, 1.0);
+        states.put(186, 0.0);
+        states.put(187, 0.0);
+        states.put(188, 0.0);
+        states.put(189, 1.0);
+        states.put(190, 0.0);
+        states.put(191, 0.0);
+        states.put(192, 1.0);
+        states.put(193, 1.0);
+        states.put(194, 2.0);
+        states.put(195, 2.0);
+        states.put(196, 2.0);
+        states.put(197, 2.0);
+        states.put(198, 2.0);
+        states.put(199, 1.0);
+        states.put(200, 1.0);
+
+
+
+        state.increaseOverallCounter();
+
+        if (state.getIteration() > 124) {
+            Integer currentstate = 0;
+            if (!state.getDirection().equals("up")) {
+                currentstate = Integer.parseInt(state.getDirection());
+            }
+
+            currentstate++;
+
+            if ((currentstate - 3) > states.size()) {
+                currentstate = 1;
+            }
+
+            state.setAmount(states.get(currentstate).intValue());
+            state.setDirection(currentstate.toString());
+            state.setIteration(1);
+            return state;
+        }
+
+
+
+        state.increaseIteration();
+        return state;
+    }
+}
